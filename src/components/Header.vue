f649b2863697d5df3ff82c5118c6c0e8d3d5107a
mbollam1699 <144732581+mbollam1699@users.noreply.github.com>
2023-10-18 20:16:09 -0400

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button 
+                :text="showAddTask ? 'Close': 'Add Task'" 
+                :color="showAddTask ? 'red': 'green'" 
+                @click="$emit('show-addTask')"/>
+    </header>
+</template>
+
+<script>
+    import Button from './Button'
+    export default {
+        name: "Header-view",
+        components: {
+            Button,
+        },
+        props: {
+            title: String,
+            showAddTask: Boolean,
+             /*, OR as below*/
+            //title: {
+            //    type: String,
+            //    default:"Hello World"
+            //}
+        }
+    }
+
+</script>
+
+<style scoped>
+    header{
+        display:flex;
+        justify-content:space-between;
+        align-content:center;
+        margin-bottom:20px;
+    }
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,109 @@
+<template>
+    <div v-if="showAddTask">
+        <AddTask @add-task="addTask" />
+    </div>
+
+    <Tasks @delete-task="deleteTask" @toggle-reminder="toggleReminder" :tasks="tasks" />
+
+</template>
+<script>
+    import Tasks from '../components/Tasks';
+    import AddTask from '../components/AddTask';
+
+    export default {
+        name: 'home-component',
+        props: {
+            showAddTask:Boolean,
+        },
+        components: {
+            Tasks,
+            AddTask,
+        },
+
+        data() {
+            return {
+                tasks: [],
+
+            }
+        },
+
+        methods: {
+            async deleteTask(id) {
+                /*  console.log("In App.vue", id);*/
+
+                if (confirm("Are you sure ?")) {
+
+                    const res = await fetch(`api/tasks/${id}`, {
+
+                        method: 'DELETE',
+                    })
+
+                    res.status === 200 ?
+                        (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error deleting task')
+                }
+            },
+
+           
+
+            async addTask(task) {
+                const res = await fetch('api/tasks', {
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json',
+                    },
+                    body: JSON.stringify(task),
+                })
+                const data = await res.json()
+                this.tasks = [...this.tasks, data]
+            },
+            async toggleReminder(id) {
+                const taskToToggle = await this.fetchTask(id)
+                const updTask = {
+                    ...taskToToggle, reminder: !taskToToggle
+                        .reminder
+                }
+                const res = await fetch(`api/tasks/${id}`, {
+
+                    method: 'PUT',
+                    headers: {
+
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(updTask)
+
+
+                })
+                const data = await res.json()
+                this.tasks = this.tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task)
+            },
+
+
+            async fetchTasks() {
+
+                const res = await fetch('api/tasks')
+
+                const data = await res.json()
+                return data
+
+            },
+
+            async fetchTask(id) {
+                const res = await fetch(`api/tasks/${id}`);
+
+                const data = await res.json()
+
+                return data
+            }
+        },
+        async created() {
+            this.tasks = await this.fetchTasks()
+        }
+
+
+    }
+   
+</script>
+
+<style>
+
+</style>
